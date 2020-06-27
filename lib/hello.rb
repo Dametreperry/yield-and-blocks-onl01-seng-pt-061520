d7542e5184641da61b1bfa1a245325c5c38fa695
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-27 04:25:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,11 @@
-
-
-
-
-
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+end
 
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
@@ -11,5 +14,5 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   end
 end
 
-# call your method here!
+
 
